9d82f26f6145214eb703428b599d6d1967ab92f3
Manoel-Mieiro <mjlmieiro@gmail.com>
2024-05-11 12:23:35 -0300

    Database V3

--- MF_Sales_DB.sql
+++ MF_Sales_DB.sql
@@ -7,7 +7,12 @@ CREATE TABLE "pedidos" (
   "total_carrinho" integer,
   "forma_pagamento" varchar(29),
   "cumpom" varchar(12),
-  "total_a_pagar" integer
+  "valor_desconto" integer,
+  "total_a_pagar" integer,
+  "confirmacao_pagamento" boolean,
+  "added_at" datetime,
+  "updated_at" datetime,
+  "deteled_at" datetime
 );
 
 CREATE TABLE "entrega" (
@@ -19,7 +24,10 @@ CREATE TABLE "entrega" (
   "numero" integer,
   "uf" varchar(2),
   "cidade" varchar(30),
-  "bairro" varchar(50)
+  "bairro" varchar(50),
+  "added_at" datetime,
+  "updated_at" datetime,
+  "deteled_at" datetime
 );
 
 CREATE TABLE "produtos" (
@@ -41,7 +49,10 @@ CREATE TABLE "produtos" (
   "altura_folha" integer,
   "comprimento_folha" integer,
   "valor_compra" integer,
-  "valor_venda" integer
+  "valor_venda" integer,
+  "added_at" datetime,
+  "updated_at" datetime,
+  "deteled_at" datetime
 );
 
 CREATE TABLE "clientes" (
@@ -50,7 +61,11 @@ CREATE TABLE "clientes" (
   "cpf" varchar(11),
   "email" varchar(80),
   "telefone" varchar(11),
-  "cartao" varchar(30)
+  "cartao" varchar(30),
+  "senha" varchar(30),
+  "added_at" datetime,
+  "updated_at" datetime,
+  "deteled_at" datetime
 );
 
 CREATE TABLE "cartoes" (
@@ -61,7 +76,10 @@ CREATE TABLE "cartoes" (
   "mes_validade" integer,
   "ano_validade" integer,
   "nome_titular" varchar(40),
-  "cod_verificacao" integer
+  "cod_verificacao" integer,
+  "added_at" datetime,
+  "updated_at" datetime,
+  "deteled_at" datetime
 );
 
 CREATE TABLE "avaliacao_pedido" (
@@ -69,14 +87,21 @@ CREATE TABLE "avaliacao_pedido" (
   "usuario" integer,
   "produto" integer,
   "qtd_estrelas" varchar(10),
-  "descricao" varchar(100)
+  "descricao" varchar(100),
+  "added_at" datetime,
+  "updated_at" datetime,
+  "deteled_at" datetime
 );
 
 CREATE TABLE "carrinho" (
   "id" integer PRIMARY KEY,
   "pedido" integer,
   "produto" integer,
-  "preco_unitario" integer
+  "quantidade" integer,
+  "preco_total_produto" integer,
+  "added_at" datetime,
+  "updated_at" datetime,
+  "deteled_at" datetime
 );
 
 CREATE TABLE "fornecedores" (
@@ -84,8 +109,13 @@ CREATE TABLE "fornecedores" (
   "nome" varchar(100),
   "cnpj" varchar(30),
   "unidade" varchar(80),
+  "cep" varchar(11),
+  "cidade" varchar(30),
+  "estado" varchar(30),
   "uf" varchar(2),
-  "prod_id" integer
+  "added_at" datetime,
+  "updated_at" datetime,
+  "deteled_at" datetime
 );
 
 CREATE TABLE "funcionario" (
@@ -95,7 +125,10 @@ CREATE TABLE "funcionario" (
   "nome" varchar(80),
   "email" varchar(80),
   "telefone" varchar(11),
-  "requisicao_compra" integer
+  "requisicao_compra" integer,
+  "added_at" datetime,
+  "updated_at" datetime,
+  "deteled_at" datetime
 );
 
 CREATE TABLE "aquisicao" (
@@ -103,7 +136,10 @@ CREATE TABLE "aquisicao" (
   "fornecedor" ineteger,
   "comprador" integer,
   "preco_total" integer,
-  "total_a_pagar" integer
+  "total_a_pagar" integer,
+  "added_at" datetime,
+  "updated_at" datetime,
+  "deteled_at" datetime
 );
 
 CREATE TABLE "comissao" (
@@ -111,17 +147,23 @@ CREATE TABLE "comissao" (
   "produto" integer,
   "preco_unitario" integer,
   "fornecedor" integer,
-  "aquisicao" integer
+  "aquisicao" integer,
+  "added_at" datetime,
+  "updated_at" datetime,
+  "deteled_at" datetime
 );
 
 CREATE TABLE "historico" (
   "id" integer PRIMARY KEY,
-  "cep" varchar(10),
-  "address" varchar(150),
-  "numero" integer,
-  "uf" varchar(2),
-  "cidade" varchar(30),
-  "bairro" varchar(50)
+  "pedido" integer,
+  "data_pedido" datetime,
+  "cliente" integer,
+  "carrinho" integer,
+  "forma_pagamento" varchar(29),
+  "total_a_pagar" integer,
+  "added_at" datetime,
+  "updated_at" datetime,
+  "deteled_at" datetime
 );
 
 CREATE TABLE "armazem" (
@@ -131,12 +173,41 @@ CREATE TABLE "armazem" (
   "address" varchar(150),
   "numero" integer,
   "uf" varchar(2),
+  "capacidade_total" integer,
   "cidade" varchar(30),
-  "bairro" varchar(50)
+  "bairro" varchar(50),
+  "added_at" datetime,
+  "updated_at" datetime,
+  "deteled_at" datetime
 );
 
 CREATE TABLE "nota_fiscal" (
-  "id" integer PRIMARY KEY
+  "id" integer PRIMARY KEY,
+  "nome_empresa" varchar(70),
+  "entrada_saida" boolean,
+  "numero_nf" varchar(70),
+  "chave_acesso" integer,
+  "natureza_operacao" integer,
+  "protocolo" varchar(60),
+  "cnpj" integer,
+  "data_emissao" datetime,
+  "address" varchar(150),
+  "numero" integer,
+  "uf" varchar(2),
+  "cidade" varchar(30),
+  "bairro" varchar(50),
+  "cep" integer,
+  "telefone" integer,
+  "base_icms" integer,
+  "valor_icms" integer,
+  "valor_frete" integer,
+  "desconto" integer,
+  "total_tributos" integer,
+  "total_produtos" integer,
+  "total_nf" integer,
+  "added_at" datetime,
+  "updated_at" datetime,
+  "deteled_at" datetime
 );
 
 ALTER TABLE "pedidos" ADD FOREIGN KEY ("cliente") REFERENCES "clientes" ("id");
@@ -177,5 +248,7 @@ ALTER TABLE "produtos" ADD FOREIGN KEY ("id") REFERENCES "comissao" ("produto");
 ALTER TABLE "aquisicao" ADD FOREIGN KEY ("id_compra") REFERENCES "comissao" ("aquisicao");
 
 ALTER TABLE "fornecedores" ADD FOREIGN KEY ("id") REFERENCES "comissao" ("fornecedor");
+
+ALTER TABLE "pedidos" ADD FOREIGN KEY ("id_pedido") REFERENCES "historico" ("pedido");
 
 ALTER TABLE "pedidos" ADD FOREIGN KEY ("id_pedido") REFERENCES "nota_fiscal" ("id");
