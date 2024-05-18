afbe6d3e454c6de51ea711f212afe104b2855e99
Manoel-Mieiro <mjlmieiro@gmail.com>
2024-05-17 21:30:29 -0300

    Added Documentation V1

--- indexes/idx003_entrega.sql
+++
@@ -1,4 +0,0 @@
-CREATE INDEX idx_tipo_entrega ON entrega (tipo_entrega) WHERE tipo_entrega = "Standard"; --Espera-se que boa parte das linhas da tabela entrega tenham como tipo da entrega Standard por ser uma opção mais barata e confortável para o cliente
-CREATE INDEX idx_uf ON entrega (uf) WHERE uf IN ('SP', 'MG', 'RJ', 'SC'); --As UFs em questão devem representar a maioria por serem as mais ricas e populosas, por conseguinte com mais propensão a adiquirir nossos produtos e portanto fazer requisição de entrega
-
-
