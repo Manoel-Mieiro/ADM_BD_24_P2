324e5458bc78c96052daadc211798cdf734ffd18
Manoel-Mieiro <mjlmieiro@gmail.com>
2024-06-10 22:46:59 -0300

    Added trigger for selling value assessment

---
+++ triggers/calcular_val_venda.sql
@@ -0,0 +1,14 @@
+-- Calcula e preenche o valor da coluna valor de venda da tabela produto baseado em um dado percentual estabelecido
+CREATE OR REPLACE FUNCTION assess() RETURNS TRIGGER AS $$
+DECLARE
+    percentual real := 0.31;
+BEGIN
+    NEW.valor_venda := NEW.valor_compra*(1+percentual);
+    RETURN NEW.valor_compra;
+END;
+$$ LANGUAGE PLPGSQL;
+
+CREATE OR REPLACE TRIGGER calc_val_venda
+BEFORE INSERT ON produtos
+FOR EACH ROW
+EXECUTE FUNCTION assess();
