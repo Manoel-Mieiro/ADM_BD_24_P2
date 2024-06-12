3b2b45974b320924e7e0db123294ab46b0e51a47
Manoel-Mieiro <mjlmieiro@gmail.com>
2024-06-12 02:13:57 -0300

    Added cart trigger that calculates product's total price base on quantity

--- triggers/calcular_produto_carrinho.sql
+++ triggers/calcular_produto_carrinho.sql
@@ -1,11 +1,11 @@
 CREATE OR REPLACE FUNCTION total_prod_cart() RETURNS TRIGGER AS $$
 BEGIN
-    INSERT INTO carrinho(total_produtos)VALUES(quantidade*(SELECT valor_venda FROM produtos as p WHERE NEW.produto = p.id))
+    NEW.preco_total_produto := NEW.quantidade * (SELECT valor_venda FROM produtos WHERE id = NEW.produto);
     RETURN NEW;
 END;
-$$ LANGUAGE PLPGSQ;
+$$ LANGUAGE PLPGSQL;
 
 CREATE OR REPLACE TRIGGER calc_prod
 BEFORE INSERT ON carrinho
-FOR EACH rows
-EXECUTE total_prod_cart();
+FOR EACH ROW
+EXECUTE FUNCTION total_prod_cart();
